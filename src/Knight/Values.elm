c70d3a53c25f299735823df1ae0eaf0a53e82cc7
Tony Sæle <tony@globalit.no>
2017-02-09 10:42:23 +0100

    adjust most damage values

--- src/Knight/Values.elm
+++ src/Knight/Values.elm
@@ -14,15 +14,15 @@ attacks =
   , swordFinish = 477 / 1.24
   , swordCharge = 623 / 1.24
   , swordChargeFinish = 668 / 1.24
-  , swordSpecial = 266 / 1.24
+  , swordSpecial = 268 / 1.24
 
   , civ = 315
   , civHeavy = 353
 
-  , swordLight = 358 / 1.24
-  , swordLightFinish = 411 / 1.24
-  , swordLightCharge = 537 / 1.24
-  , swordLightChargeFinish = 587 / 1.24
+  , swordLight = 361 / 1.24
+  , swordLightFinish = 415 / 1.24
+  , swordLightCharge = 541 / 1.24
+  , swordLightChargeFinish = 591 / 1.24
 
   , swordHeavy = 477 / 1.24
   , swordHeavyFinish = 548 / 1.24
@@ -37,21 +37,21 @@ attacks =
   , brandishHeavy = 656 / 1.24
   , brandishHeavyFinish = 774 / 1.24
   , brandishHeavyCharge = 1040 / 1.24
-  , brandishHeavySpecial = 480 / 1.24
+  , brandishHeavySpecial = 484 / 1.24
 
-  , fang = 566 / 1.24
-  , fangFinish = 666 / 1.24
-  , fangCharge = 897 / 1.24
+  , fang = 570 / 1.24
+  , fangFinish = 671 / 1.24
+  , fangCharge = 904 / 1.24
 
 -- Guns --
   , blaster = 314 / 1.25
-  , blasterCharge = 532 / 1.24
+  , blasterCharge = 535 / 1.24
 
-  , nova = 336 / 1.24
-  , novaCharge = 572 / 1.24
+  , nova = 339 / 1.24
+  , novaCharge = 576 / 1.24
 
-  , driver = 295 / 1.24
-  , driverCharge = 501 / 1.24
+  , driver = 297 / 1.24
+  , driverCharge = 504 / 1.24
 
   , antigua = 231 / 1.24
   , antiguaCharge = 392 / 1.24
@@ -59,28 +59,28 @@ attacks =
   , pepper = (231 / 1.24) * 0.88
   , pepperCharge = (391 / 1.24) * 0.88
 
-  , magnus = 360 / 1.24
-  , magnusCharge = 611 / 1.24
+  , magnus = 362 / 1.24
+  , magnusCharge = 616 / 1.24
 
-  , neutralizer = 652 / 1.24
+  , neutralizer = 657 / 1.24
   
   , tortofist = 252 / 1.24
 
 -- Bombs --
-  , nitro = 443 / 1.24
-  , irontech = 476 / 1.24
-  , bab = 509 / 1.24
+  , nitro = 446 / 1.24
+  , irontech = 479 / 1.24
+  , bab = 512 / 1.24
 
-  , haze = 389 / 1.24
+  , haze = 392 / 1.24
   , graviton = 24 / 1.24
-  , gravitonCollapse = 423 / 1.24
+  , gravitonCollapse = 425 / 1.24
 
-  , dr = 182 / 1.24 
+  , dr = 183 / 1.24
 
-  , shardCore = 246 / 1.24
-  , shardPure = 296 / 1.24
-  , shardStatus = 271 / 1.24
+  , shardCore = 249 / 1.24
+  , shardPure = 298 / 1.24
+  , shardStatus = 273 / 1.24
 
   , shardOld = 593 / 1.24
-  , saltOld = 304
+  , saltOld = 305
   }
